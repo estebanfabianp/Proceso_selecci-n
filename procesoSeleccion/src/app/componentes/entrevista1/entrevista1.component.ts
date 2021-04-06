c483cba8d59ddfafe3b228deb4c489f4646b6485
estebanfabianp <38668876+estebanfabianp@users.noreply.github.com>
2021-04-05 20:42:29 -0500

    maquetacion
    maquetacion de entrevista

--- procesoSeleccion/src/app/componentes/entrevista1/entrevista1.component.ts
+++ procesoSeleccion/src/app/componentes/entrevista1/entrevista1.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EntrevistaModule } from '../../modelos/entrevista.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-entrevista1',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Entrevista1Component implements OnInit {
 
-  constructor() { }
+  constructor(    private mostrar: EntrevistaModule,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+/*
+
+
+import { MostrarVacantesService } from '../../servicios/mostrar-vacantes.service';
+
+@Component({
+  selector: 'app-mostrar-vacantes',
+  templateUrl: './mostrar-vacantes.component.html',
+  styleUrls: ['./mostrar-vacantes.component.css'],
+})
+export class MostrarVacantesComponent implements OnInit {
+  constructor(
+
+  ) {}
+
+  
+  
+
+  selecVacantes = new VacanteModule();
+
+  ngOnInit() {
+    this.mostrar.mostrarVacantes().subscribe((resp) => {
+      this.vacantes = resp;
+    });
+  }
+
+  onSelect(vacantes: VacanteModule) {
+    this.selecVacantes = vacantes;
+  }
+}
+*/
